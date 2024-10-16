887394c1a8e814eea7b11ec47c50a0a769984c37
TimMov <tgasimov6@mail.ru>
2024-10-16 21:43:31 +0300

    Разнесение функционала по отдельным функциям

--- main.go
+++ main.go
@@ -15,17 +15,7 @@ type Task struct {
 	Completed   bool   `json:"completed"`   // Статус задачи (выполнена или нет)
 }
 
-//var Tasks []Task
-
 func main() {
-
-	fmt.Println("Open file")
-
-	//file, err := os.OpenFile("TestFile.json", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
-
-	//if err != nil {
-	//	panic(err)
-	//}
 
 	fmt.Println("запись в формате команда - задача - описание задачи: ")
 
@@ -39,165 +29,27 @@ func main() {
 
 	if strings.Contains(input, "add") {
 
-		words := strings.Split(input, " - ")
-
-		i := Task{
-			ID:          words[3],
-			Name:        words[1],
-			Description: words[2],
-			Completed:   false,
-		}
-
-		var tasks []Task
-
-		dataFile, err := os.ReadFile("TestFile.json")
-
-		err = json.Unmarshal(dataFile, &tasks)
-
-		if err != nil {
-			fmt.Println("Ошибка:", err)
-			return
-		}
-
-		CheckTasks := SearchFile(tasks, words[3])
-
-		if CheckTasks != nil {
-			fmt.Println("ID же есть:", CheckTasks.ID)
-			return
-		}
-
-		var founTasks []Task
-
-		founTasks = append(founTasks, i)
-
-		for Num := 0; Num < len(tasks); Num++ {
-			founTasks = append(founTasks, tasks[Num])
-		}
-
-		//founTasks = append(founTasks, i, tasks[0])
-
-		err = clearJSONfile("TestFile.json", founTasks)
-
-		//data, err := json.Marshal(founTasks)
-
-		if err != nil {
-			fmt.Println("Ошибка:", err)
-			return
-		}
-
-		//_, err = file.Write(data)
-
-		fmt.Println("Save file!")
-
-		//if err != nil {
-		//	fmt.Println("Ошибка при записи в файл:", err)
-		//	return
-		//}
+		addTask(input)
 
 	}
 
 	if strings.Contains(input, "complete") {
 
-		words := strings.Split(input, " ")
-
-		var tasks []Task
-
-		data, err := os.ReadFile("TestFile.json")
-		if err != nil {
-			fmt.Println("Ошибка чтения файла:", err)
-			return
-		}
-
-		err = json.Unmarshal(data, &tasks)
-
-		if err != nil {
-			fmt.Println("Ошибка десериализации JSON:", err)
-			return
-		}
-
-		otvet := SearchFile(tasks, words[1])
-
-		//otvet[0].Completed = true
-
-		if otvet != nil {
-			for Num := 0; Num < len(tasks); Num++ {
-				if tasks[Num].ID == otvet.ID {
-					tasks[Num].Completed = true
-					break
-				}
-			}
-
-			err = clearJSONfile("TestFile.json", tasks)
-
-		} else {
-			fmt.Println("ID не найден")
-		}
+		completeTask(input)
 
 	}
 
 	if strings.Contains(input, "delete") {
 
-		words := strings.Split(input, " ")
-
-		var tasks []Task
-
-		data, err := os.ReadFile("TestFile.json")
-		if err != nil {
-			fmt.Println("Ошибка чтения файла:", err)
-			return
-		}
-
-		err = json.Unmarshal(data, &tasks)
-
-		if err != nil {
-			fmt.Println("Ошибка десериализации JSON:", err)
-			return
-		}
-
-		var DelTasks []Task
-
-		for i := 0; i < len(tasks); i++ {
-
-			if tasks[i].ID != words[1] {
-
-				DelTasks = append(DelTasks, tasks[i])
-
-			}
-
-		}
-
-		clearJSONfile("TestFile.json", DelTasks)
+		deleteTask(input)
 
 	}
 
 	if strings.Contains(input, "show") {
 
-		words := strings.Split(input, " ")
-
-		var tasks []Task
-
-		data, err := os.ReadFile("TestFile.json")
-		if err != nil {
-			fmt.Println("Ошибка чтения файла:", err)
-			return
-		}
-
-		err = json.Unmarshal(data, &tasks)
-
-		if err != nil {
-			fmt.Println("Ошибка десериализации JSON:", err)
-			return
-		}
-
-		for i := 0; i < len(tasks); i++ {
-			if tasks[i].ID == words[1] {
-				fmt.Println(tasks[i].Name, tasks[i].Description, tasks[i].Completed)
-			}
-		}
+		showTask(input)
 
 	}
-
-	fmt.Println("Close file!")
 
 }
 
@@ -236,5 +88,152 @@ func clearJSONfile(filename string, Massive []Task) error {
 	}
 
 	return nil
+
+}
+
+func addTask(input string) {
+
+	words := strings.Split(input, " - ")
+
+	i := Task{
+		ID:          words[3],
+		Name:        words[1],
+		Description: words[2],
+		Completed:   false,
+	}
+
+	var tasks []Task
+
+	dataFile, err := os.ReadFile("TestFile.json")
+
+	err = json.Unmarshal(dataFile, &tasks)
+
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		return
+	}
+
+	CheckTasks := SearchFile(tasks, words[3])
+
+	if CheckTasks != nil {
+		fmt.Println("ID же есть:", CheckTasks.ID)
+		return
+	}
+
+	var founTasks []Task
+
+	founTasks = append(founTasks, i)
+
+	for Num := 0; Num < len(tasks); Num++ {
+		founTasks = append(founTasks, tasks[Num])
+	}
+
+	err = clearJSONfile("TestFile.json", founTasks)
+
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+		return
+	}
+
+	fmt.Println("Save file!")
+
+}
+
+func completeTask(input string) {
+
+	words := strings.Split(input, " ")
+
+	var tasks []Task
+
+	data, err := os.ReadFile("TestFile.json")
+	if err != nil {
+		fmt.Println("Ошибка чтения файла:", err)
+		return
+	}
+
+	err = json.Unmarshal(data, &tasks)
+
+	if err != nil {
+		fmt.Println("Ошибка десериализации JSON:", err)
+		return
+	}
+
+	otvet := SearchFile(tasks, words[1])
+
+	if otvet != nil {
+		for Num := 0; Num < len(tasks); Num++ {
+			if tasks[Num].ID == otvet.ID {
+				tasks[Num].Completed = true
+				break
+			}
+		}
+
+		err = clearJSONfile("TestFile.json", tasks)
+
+	} else {
+		fmt.Println("ID не найден")
+	}
+
+}
+
+func deleteTask(input string) {
+
+	words := strings.Split(input, " ")
+
+	var tasks []Task
+
+	data, err := os.ReadFile("TestFile.json")
+	if err != nil {
+		fmt.Println("Ошибка чтения файла:", err)
+		return
+	}
+
+	err = json.Unmarshal(data, &tasks)
+
+	if err != nil {
+		fmt.Println("Ошибка десериализации JSON:", err)
+		return
+	}
+
+	var DelTasks []Task
+
+	for i := 0; i < len(tasks); i++ {
+
+		if tasks[i].ID != words[1] {
+
+			DelTasks = append(DelTasks, tasks[i])
+
+		}
+
+	}
+
+	clearJSONfile("TestFile.json", DelTasks)
+
+}
+
+func showTask(input string) {
+
+	words := strings.Split(input, " ")
+
+	var tasks []Task
+
+	data, err := os.ReadFile("TestFile.json")
+	if err != nil {
+		fmt.Println("Ошибка чтения файла:", err)
+		return
+	}
+
+	err = json.Unmarshal(data, &tasks)
+
+	if err != nil {
+		fmt.Println("Ошибка десериализации JSON:", err)
+		return
+	}
+
+	for i := 0; i < len(tasks); i++ {
+		if tasks[i].ID == words[1] {
+			fmt.Println(tasks[i].Name, tasks[i].Description, tasks[i].Completed)
+		}
+	}
 
 }
